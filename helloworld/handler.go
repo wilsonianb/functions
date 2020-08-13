3e4b995a1ff059250d9d93030d76359397711673
Brandon Wilson <brandon@coil.com>
2020-08-13 15:17:42 -0500

    new

--- helloworld/handler.go
+++ helloworld/handler.go
@@ -6,5 +6,6 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello function. You said: %s", string(req))
+	// return fmt.Sprintf("Hello function. You said: %s", string(req))
+	return fmt.Sprintf("Hello Codius")
 }
