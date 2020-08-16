a641187452083b2b6fa597b68392881085243e86
Brandon Wilson <brandon@coil.com>
2020-08-16 10:26:59 -0500

    fix checked header

--- auth/handler.js
+++ auth/handler.js
@@ -6,7 +6,7 @@ module.exports = async (event, context) => {
     'status': 'Received input: ' + JSON.stringify(event.body)
   }
 
-  if (event.headers['x-forwarded-host'] === 'wilsonianb.of.codiusfaas.net') {
+  if (event.headers['x-original-url'].indexOf('wilsonianb.of.codiusfaas.net') !== -1) {
     return context
       .status(402)
       .succeed({})
