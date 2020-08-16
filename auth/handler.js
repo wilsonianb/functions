96526d67baba99cadf6e6eba965e7cd7fc0d5bb9
Brandon Wilson <brandon@coil.com>
2020-08-16 10:02:58 -0500

    return 402

--- auth/handler.js
+++ auth/handler.js
@@ -6,6 +6,11 @@ module.exports = async (event, context) => {
     'status': 'Received input: ' + JSON.stringify(event.body)
   }
 
+  if (event.headers['x-forwarded-host'] === 'wilsonianb.of.codiusfaas.net') {
+    return context
+      .status(402)
+      .succeed({})
+  }
   return context
     .status(200)
     .succeed(result)
